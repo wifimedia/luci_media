0ad3eeea4430de78e9ce28140bcc3221fc0466de
wifimedia <dungtd8x@gmail.com>
2020-01-14 09:49:01 +0700

    KRACK
    		translate("Enable key reinstallation (KRACK) countermeasures"),
    		translate("Complicates key reinstallation attacks on the client side by disabling retransmission of EAPOL-Key frames that are used to install keys. This workaround might cause interoperability issues and reduced robustness of key negotiation especially in environments with heavy traffic load."))

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
@@ -1000,8 +1000,7 @@ if hwtype == "mac80211" then
 	end
 	]]--
 	local key_retries = s:taboption("encryption", Flag, "wpa_disable_eapol_key_retries",
-		translate("Enable key reinstallation (KRACK) countermeasures"),
-		translate("Complicates key reinstallation attacks on the client side by disabling retransmission of EAPOL-Key frames that are used to install keys. This workaround might cause interoperability issues and reduced robustness of key negotiation especially in environments with heavy traffic load."))
+		translate("Enable key reinstallation (KRACK) countermeasures"))
 
 	key_retries:depends({mode="ap", encryption="wpa2"})
 	key_retries:depends({mode="ap", encryption="psk2"})
